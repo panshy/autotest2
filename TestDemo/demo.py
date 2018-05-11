36b479e6f1463f30bb89c5f74d2ac42481eb1531
panshy <332300639@qq.com>
2018-05-11 10:15:31 +0800

    demo 提交

---
+++ TestDemo/demo.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+
+from selenium import webdriver
+
+browser = webdriver.Chrome()
+browser.get("http://10.6.2.80:5002")
+loginusername = browser.find_element_by_xpath("//input[@type='text']")
+loginuserpasswd = browser.find_element_by_xpath("//input[@type='password']")
+loginsubmit = browser.find_element_by_id("btn")
+loginusername.send_keys("root")
+loginuserpasswd.send_keys("scutech")
+loginsubmit.click()
